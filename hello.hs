16dad894bfa22992ba4784339f7a8fef66b306fd
nmreadelf <frank.q.kong@hotmail.com>
2023-08-21 11:08:23 +0800

    Split some code into Html.hs
    - add Html module

--- hello.hs
+++ hello.hs
@@ -1,41 +1,7 @@
-newtype Html = Html String
+import Html
 
-newtype Structure = Structure String
-
-type Title = String
-
-el :: String -> String -> String
-el tag content = 
-  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
-
-html_ :: Title -> Structure -> Html
-html_ title content =
-  Html
-    (el "html"
-      (el "head" (el "title" title)
-        <> el "body" (getStructureString content)
-      )
-    )
-
-p_ :: String -> Structure
-p_ = Structure . el "p"
-
-h1_ :: String -> Structure
-h1_ = Structure . el "h1"
-
-getStructureString :: Structure -> String
-getStructureString content =
-  case content of
-    Structure str -> str
-
-append_ :: Structure -> Structure -> Structure
-append_ c1 c2 =
-  Structure (getStructureString c1 <> getStructureString c2)
-
-render :: Html -> String
-render html =
-  case html of
-    Html str -> str
+main :: IO()
+main = putStrLn (render myhtml)
 
 myhtml :: Html
 myhtml =
@@ -49,5 +15,3 @@ myhtml =
       )
     )
 
-main :: IO()
-main = putStrLn (render myhtml)
